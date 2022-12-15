c51ca389b47415ccfb61b529e3a0f49852acfcc1
AlexisRsgl <alexis.rsgl@laposte.net>
2022-12-15 10:40:04 +0100

    update css inscription

--- Inscription.php
+++ Inscription.php
@@ -1,9 +1,5 @@
 <?php 
-
-
 require 'configadmin.php';
-
-var_dump($dbh);
 
 $classes = $dbh->query('SELECT * FROM classe');
 
@@ -13,15 +9,11 @@ if (!empty($_POST['nom']) && !empty($_POST['prenom'])){
   $ip_add = $_SERVER['REMOTE_ADDR'];
   $classe=$_POST['ID_classe'];
 
-
-
-
-
-
   $inscription = $dbh->prepare('INSERT INTO eleve(Nom,Prenom,AdressIP,ID_classe) value (?,?,?,?) ');
   $inscription->execute([$nom,$prenom,$ip_add,$classe]);
+  header('Location: prof_choice .php');
+  exit();
 }
-  
 ?>
 
 <!doctype html>
@@ -29,39 +21,43 @@ if (!empty($_POST['nom']) && !empty($_POST['prenom'])){
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="img/inscription_logo.png"/>
     <title>Inscription</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;500;600;700&family=Ubuntu:wght@300;400;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   </head>
-  <body>
+  <header>
+  <div class="title_header">
     <h1>Inscription</h1>
-
+  </div>
+  </header>
+  <body>
     <form method="post">
-  <div class="mb-3">
-    <label  class="form-label">Nom</label>
-    <input type="text" name="nom" class="form-control">
-  </div>
-  <div class="mb-3">
-    <label  class="form-label">Prenom</label>
-    <input type="text" name="prenom" class="form-control">
-  </div>
-
-  <select class="form-select" aria-label="Default select example" name="ID_classe">
-<?php foreach ($classes as $classe): ?>
-    <option value="<?= $classe['ID_classe']?>"><?= $classe['ID_classe']?></option>
-    <?php endforeach; ?>
-    
-
-</select>
-
-  <button type="submit" class="btn btn-primary">Envoyer</button>
-</form>
-
-
-
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-  </body>
-
- 
+      <div class="container_form">
+        <div class="mb-3_pp">
+          <label  class="form-label">Nom</label>
+          <input type="text" name="nom" class="form-control" placeholder="Nom">
+        </div>
+        <div class="mb-3_pp">
+          <label  class="form-label">Prenom</label>
+          <input type="text" name="prenom" class="form-control" placeholder="Prenom">
+        </div>
+        <div class="mb-3_pp">
+        <select class="form-select" aria-label="Default select example" name="ID_classe">
+          <option>Classe</option>
+          <?php foreach ($classes as $classe): ?>
+          <option value="<?= $classe['ID_classe']?>"><?= $classe['ID_classe']?></option>
+          <?php endforeach; ?>
+        </select>
+        </div>
+          <div class="login_btn">
+            <button type="submit" class="btn btn-dark">S'inscrire</button>
+          </div>
+      </div>
+    </form>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
 </html>
